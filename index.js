6b7d57173590dce858cdc3bc2c59515fcaee5a42
Adrià Pallejà <adria.palleja@gmail.com>
2018-10-03 16:48:49 +0200

    Temporary delete static files

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const server = http.Server(app);
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const router = require('./router.js');
+const fs = require('fs');
 require('./db');
 require('./socket')(server);
 const port = process.env.PORT || 3000;
@@ -13,7 +14,23 @@ app
   .use(cors())
   .use(bodyParser.urlencoded({ extended: false }))
   .use(bodyParser.json())
-  .use(express.static(__dirname + '/../werewolf-front-end/werewolf-frontend/dist/werewolf-frontend'))
+  // .use((req,res)=>{
+  //   if (req.method === 'GET') {
+  //     fs.readFile(__dirname + '/dist/werewolf-frontend/index.html', function (err, data) {
+  //       if (err) {
+  //         res.statusCode = 500;
+  //         res.end(`Error getting the file: ${err}.`);
+  //       } else {
+  //         // based on the URL path, extract the file extention. e.g. .js, .doc, ...
+  //         res.statusCode = 200;
+  //         // if the file is found, set Content-type and send data
+  //         res.setHeader('Content-type', 'text/html' );
+  //         res.end(data);
+  //       }
+  //     });
+  //   }
+  // })
+  // .use(express.static(__dirname + '/dist/werewolf-frontend'))
   .use(router);
   
 server.listen(port, () => {
